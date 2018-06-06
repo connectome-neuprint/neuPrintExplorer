657cdac6bfa0b3da4d38e5df77cc76f3bbb4c85e
Stephen Plaza <plazas@janelia.hhmi.org>
2018-06-05 22:49:11 -0400

    adds cards to front page to provide useful information

--- src/js/components/Home.react.js
+++ src/js/components/Home.react.js
@@ -9,6 +9,9 @@ import { Redirect } from 'react-router-dom';
 import _ from "underscore";
 import PropTypes from 'prop-types';
 import {withStyles} from 'material-ui/styles';
+import Card from 'material-ui/Card';
+import { CardContent } from 'material-ui/Card';
+import Grid from 'material-ui/Grid';
 
 const styles = () => ({
     root: {
@@ -19,6 +22,19 @@ const styles = () => ({
     roottext: {
         maxWidth: 560,
         margin: "0 auto"
+    },
+    card: {
+        minWidth: 275,
+        maxWidth: 500,
+        marginLeft: "auto"
+    },
+    card2: {
+        minWidth: 275,
+        maxWidth: 500,
+    },
+    title: {
+        marginBottom: 16,
+        fontSize: 14,
     },
 });
 
@@ -42,15 +58,70 @@ class Home extends React.Component {
         return (
             <div className={classes.root}>
                 {redirectHome ? <Redirect to="/" /> : <div / >}
-                <div className={classes.roottext}>
-                    <Typography variant="title">
-                        Analysis tools for conenctomics
-                    </Typography>
-                    <br />
-                    <Typography variant="body1">
-                        neuPrintExplorer provides tools to query and visualize connectomic data stored in  <a href="https://github.com/janelia-flyem/neuPrint">neuPrint</a>, which uses a neo4j graph database.
-                    </Typography>
-                </div>
+                <Grid 
+                        container 
+                        spacing={24}
+                >
+                    <Grid 
+                            item 
+                            xs={12}
+                    >
+                        <div className={classes.roottext}>
+                            <Typography variant="title">
+                                Analysis tools for conenctomics
+                            </Typography>
+                            <br />
+                            <Typography variant="body1">
+                                neuPrintExplorer provides tools to query and visualize connectomic data stored in  <a href="https://github.com/janelia-flyem/neuPrint">neuPrint</a>, which uses a neo4j graph database.
+                            </Typography>
+                        </div>
+                    </Grid>
+                    <Grid 
+                            item 
+                            xs={12}
+                            sm={6}
+                    >
+                        <Card className={classes.card}>
+                            <CardContent>
+                              <Typography
+                                            className={classes.title}
+                                            color="textSecondary"
+                              >
+                               neuPrint Server Information 
+                              </Typography>
+                              <Typography component="p">
+                                available datasets: <br /> 
+                              </Typography>
+                              <Typography component="p">
+                                last modified: <br /> 
+                              </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid 
+                            item 
+                            xs={12}
+                            sm={6}
+                    >
+                        <Card className={classes.card2}>
+                            <CardContent>
+                              <Typography
+                                            className={classes.title}
+                                            color="textSecondary"
+                              >
+                                Helpful Hints  
+                              </Typography>
+                              <Typography component="p">
+                                Use the search icon at the top left to query the database.  Click
+                                the help icon for detailed information on the graph model. <br />
+                              </Typography>
+                              <Typography component="p">
+                                hint: <br /> 
+                              </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
             </div>
         );
     }
