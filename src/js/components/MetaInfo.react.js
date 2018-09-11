5f979cd8c42f5695f76d7fe35af2a5ec8c7c4468
Stephen Plaza <plazas@janelia.hhmi.org>
2018-09-11 07:28:53 -0400

    clears server data when logged out

--- src/js/components/MetaInfo.react.js
+++ src/js/components/MetaInfo.react.js
@@ -13,17 +13,15 @@ import C from "../reducers/constants"
 class MetaInfo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            neoServer: "",
-            datasets: [],
-            rois: {},
-            datsasetInfo: {},
-        };
-
         this.updateDB(this.props.userInfo);
     }
    
     componentWillReceiveProps(nextProps) {
+        if (nextProps.userInfo === null && this.props.userInfo !== null) {
+            this.props.setNeoDatasets([], {}, {});
+            this.props.setNeoServer("");
+        }
+        
         if (!_.isEqual(nextProps.userInfo, this.props.userInfo)) {
             this.updateDB(nextProps.userInfo);        
         }
@@ -33,31 +31,27 @@ class MetaInfo extends React.Component {
         fetch('/api/dbmeta/datasets')
             .then(result=>result.json())
             .then(items=> {
-                let datasets = [];
-                let rois = {}
-                let datasetInfo = {}
-                for (let dataset in items) {
-                    datasets.push(dataset)
-                    rois[dataset] = items[dataset].ROIs;
-                    datasetInfo[dataset] = {
-                        uuid:   items[dataset].uuid,
-                        lastmod: items[dataset]["last-mod"]
+                if (!("error" in items)) {
+                    let datasets = [];
+                    let rois = {}
+                    let datasetInfo = {}
+                    for (let dataset in items) {
+                        datasets.push(dataset)
+                        rois[dataset] = items[dataset].ROIs;
+                        datasetInfo[dataset] = {
+                            uuid:   items[dataset].uuid,
+                            lastmod: items[dataset]["last-mod"]
+                        }
                     }
+                    this.props.setNeoDatasets(datasets, rois, datasetInfo);
                 }
-                
-                this.setState({
-                        datasets: datasets,
-                        rois: rois,
-                        datasetInfo: datasetInfo,
-                    }
-                );
-                
-                this.props.setNeoDatasets(datasets, rois, datasetInfo);
             });
         fetch('/api/dbmeta/database')
             .then(result=>result.json())
             .then(data=> {
-                this.props.setNeoServer(data.Location);
+                if (!("error" in data)) {
+                    this.props.setNeoServer(data.Location);
+                }
             });
 
     }
