51f13a083f5ad4cf8a4783e2d3154d790c974ad1
Jody Clements <jody@neomorphic.co.uk>
2018-10-09 13:29:41 -0400

    Reformatted modules to new style guide.

--- src/js/components/SimpleCell.react.js
+++ src/js/components/SimpleCell.react.js
@@ -2,48 +2,50 @@
  * Simple table cell wrapper for table info.
 */
 
-"use strict";
+'use strict';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TableCell} from 'material-ui/Table';
+import { TableCell } from 'material-ui/Table';
 import { withStyles } from 'material-ui/styles';
 
 const styles = () => ({
   fcell: {
-    height: "1px",
+    height: '1px'
   },
   basic: {
     //backgroundColor: "white",
-  },
+  }
 });
 
 class SimpleCell extends React.Component {
-    render() {
-        const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
 
-        return (
-            <TableCell
-                        className={(this.props.isSimple ? classes.basic : classes.fcell) + ((this.props.lockVal > -1) ? " lockLeft-" + this.props.lockVal.toString() : "")}
-                        padding={this.props.isSimple ? "default" : "none"} 
-
-                        style={(this.props.bgColor !== "") ? {'background': this.props.bgColor} : {}}
-            > 
-                {this.props.children}
-            </TableCell>
-        );
-    }
+    return (
+      <TableCell
+        className={
+          (this.props.isSimple ? classes.basic : classes.fcell) +
+          (this.props.lockVal > -1 ? ' lockLeft-' + this.props.lockVal.toString() : '')
+        }
+        padding={this.props.isSimple ? 'default' : 'none'}
+        style={this.props.bgColor !== '' ? { background: this.props.bgColor } : {}}
+      >
+        {this.props.children}
+      </TableCell>
+    );
+  }
 }
 
 SimpleCell.defaultProps = {
-    isSimple: true
+  isSimple: true
 };
 
 SimpleCell.propTypes = {
-    children: PropTypes.any.isRequired,
-    classes: PropTypes.object.isRequired,
-    isSimple: PropTypes.bool.isRequired,
-    lockVal: PropTypes.number.isRequired, 
-    bgColor: PropTypes.string.isRequired, 
-}
+  children: PropTypes.any.isRequired,
+  classes: PropTypes.object.isRequired,
+  isSimple: PropTypes.bool.isRequired,
+  lockVal: PropTypes.number.isRequired,
+  bgColor: PropTypes.string.isRequired
+};
 
 export default withStyles(styles)(SimpleCell);
--- src/js/helpers/SimpleCellWrapper.js
+++ src/js/helpers/SimpleCellWrapper.js
@@ -2,7 +2,7 @@
  * Wrapper to render simple cell and access table value.
 */
 
-"use strict";
+'use strict';
 
 import SimpleCell from '../components/SimpleCell.react';
 import React from 'react';
@@ -10,58 +10,52 @@ import { TableSortLabel } from 'material-ui/Table';
 import Tooltip from 'material-ui/Tooltip';
 
 export default class SimpleCellWrapper {
-    constructor(key, child, isSimple=true, value=null, lockVal=-1, bgColor="") {
-        this.key = key;
-        this.value = value;
-        this.isSimple = isSimple;
-        this.child = child;
-        this.lockVal=lockVal;
-        this.bgColor = bgColor;
-    }
+  constructor(key, child, isSimple = true, value = null, lockVal = -1, bgColor = '') {
+    this.key = key;
+    this.value = value;
+    this.isSimple = isSimple;
+    this.child = child;
+    this.lockVal = lockVal;
+    this.bgColor = bgColor;
+  }
 
-    getValue = () => {
-        if (this.value !== null) {
-            return this.value;
-        } else {
-            return this.child;
-        }
+  getValue = () => {
+    if (this.value !== null) {
+      return this.value;
+    } else {
+      return this.child;
     }
+  };
 
-    getComponent = (sortFunc=null, orderBy=-1, enableSort=false, order="desc") => {
-        if (sortFunc === null) {
-            return (<SimpleCell
-                            key={this.key}
-                            value={this.value}
-                            isSimple={this.isSimple}
-                            lockVal={this.lockVal}
-                            bgColor={this.bgColor}
-                    >
-                        {this.child}
-                    </SimpleCell>);
-        } else {
-             return (<SimpleCell
-                            key={this.key}
-                            value={this.value}
-                            isSimple={this.isSimple}
-                            lockVal={this.lockVal}
-                            bgColor={this.bgColor}
-                    >
-                        <Tooltip
-                                    title="Sort"
-                                    placement={'bottom-end'}
-                                    enterDelay={300}
-                        >
-                            <TableSortLabel
-                                            active={enableSort}
-                                            direction={order}
-                                            onClick={() => sortFunc(orderBy)}
-                            >
-                                {this.child}
-                            </TableSortLabel>
-                        </Tooltip> 
-                    </SimpleCell>);
-        }
+  getComponent = (sortFunc = null, orderBy = -1, enableSort = false, order = 'desc') => {
+    if (sortFunc === null) {
+      return (
+        <SimpleCell
+          key={this.key}
+          value={this.value}
+          isSimple={this.isSimple}
+          lockVal={this.lockVal}
+          bgColor={this.bgColor}
+        >
+          {this.child}
+        </SimpleCell>
+      );
+    } else {
+      return (
+        <SimpleCell
+          key={this.key}
+          value={this.value}
+          isSimple={this.isSimple}
+          lockVal={this.lockVal}
+          bgColor={this.bgColor}
+        >
+          <Tooltip title="Sort" placement={'bottom-end'} enterDelay={300}>
+            <TableSortLabel active={enableSort} direction={order} onClick={() => sortFunc(orderBy)}>
+              {this.child}
+            </TableSortLabel>
+          </Tooltip>
+        </SimpleCell>
+      );
     }
+  };
 }
-
-
