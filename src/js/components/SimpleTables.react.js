085995b96ad004466783809458a3db45f6e7bf16
Jody Clements <jody@neomorphic.co.uk>
2018-10-09 16:47:01 -0400

    Code cleanup and formatting.
    Also adds some todos.

--- src/js/components/SimpleTables.react.js
+++ src/js/components/SimpleTables.react.js
@@ -3,8 +3,6 @@
  * a simple table or a table of tables.
 */
 
-"use strict";
-
 import React from 'react';
 import Typography from 'material-ui/Typography';
 import SimpleTable from './SimpleTable.react';
@@ -12,7 +10,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import ExpansionPanel, {
   ExpansionPanelSummary,
-  ExpansionPanelDetails,
+  ExpansionPanelDetails
 } from 'material-ui/ExpansionPanel';
 import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
 import Table, {
@@ -20,107 +18,101 @@ import Table, {
   TableCell,
   TableFooter,
   TablePagination,
-  TableRow,
+  TableRow
 } from 'material-ui/Table';
 import TablePaginationActions from './TablePaginationActions.react';
 
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 1,
+    marginTop: theme.spacing.unit * 1
   },
   table: {
-    minWidth: 500,
+    minWidth: 500
   },
   cellborder: {
-    borderBottom: 0,
+    borderBottom: 0
   },
   nopad: {
-    padding: 0,
+    padding: 0
   }
 });
 
 class SimpleTables extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            page: 0,
-            rowsPerPage: 5,
-        };
-    }
-
-    handleChangePage = (event, page) => {
-        this.setState({ page });
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      page: 0,
+      rowsPerPage: 5
     };
+  }
 
-    handleChangeRowsPerPage = event => {
-        this.setState({ rowsPerPage: event.target.value });
-    };
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
 
-    render() {
-        const { classes } = this.props;
-        const { rowsPerPage, page } = this.state;
-        const startRecord = page * rowsPerPage;
-        
-        return (
-            <div className={classes.root}>
-            {this.props.allTables !== null ?
-                ((this.props.allTables.length != 1) ?
-                    (<Table className={classes.table}>
-                        <TableBody>
-                        {this.props.allTables.slice(startRecord, page * rowsPerPage + rowsPerPage).map( (tableinfo, index) => {
-                            return (
-                                <TableRow key={startRecord + index}>
-                                    <TableCell 
-                                                className={classes.cellborder} 
-                                                padding="none"
-                                    >
-                                        <ExpansionPanel>
-                                            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                                                <Typography>
-                                                    {tableinfo.name}
-                                                </Typography> 
-                                            </ExpansionPanelSummary>
-                                            <ExpansionPanelDetails className={classes.nopad}>
-                                                <SimpleTable data={tableinfo} />
-                                            </ExpansionPanelDetails>
-                                        </ExpansionPanel>
-                                    </TableCell>
-                                </TableRow>
-                            );
-                        })}
-                        </TableBody>
-                        <TableFooter>
-                          <TableRow>
-                            <TablePagination
-                              colSpan={1}
-                              count={this.props.allTables.length}
-                              rowsPerPage={rowsPerPage}
-                              page={page}
-                              onChangePage={this.handleChangePage}
-                              onChangeRowsPerPage={this.handleChangeRowsPerPage}
-                              Actions={TablePaginationActions}
-                            />
-                          </TableRow>
-                        </TableFooter>
-                    </Table>) :
-                    (
-                        <SimpleTable data={this.props.allTables[0]} />
-                    )
-                ) :
-                <div />
-            }
-            </div>
-        );
-    }
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { rowsPerPage, page } = this.state;
+    const startRecord = page * rowsPerPage;
+
+    return (
+      <div className={classes.root}>
+        {this.props.allTables !== null ? (
+          this.props.allTables.length !== 1 ? (
+            <Table className={classes.table}>
+              <TableBody>
+                {this.props.allTables
+                  .slice(startRecord, page * rowsPerPage + rowsPerPage)
+                  .map((tableinfo, index) => {
+                    return (
+                      <TableRow key={startRecord + index}>
+                        <TableCell className={classes.cellborder} padding="none">
+                          <ExpansionPanel>
+                            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                              <Typography>{tableinfo.name}</Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails className={classes.nopad}>
+                              <SimpleTable data={tableinfo} />
+                            </ExpansionPanelDetails>
+                          </ExpansionPanel>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TablePagination
+                    colSpan={1}
+                    count={this.props.allTables.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={this.handleChangePage}
+                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                    Actions={TablePaginationActions}
+                  />
+                </TableRow>
+              </TableFooter>
+            </Table>
+          ) : (
+            <SimpleTable data={this.props.allTables[0]} />
+          )
+        ) : (
+          <div />
+        )}
+      </div>
+    );
+  }
 }
 
 SimpleTables.propTypes = {
-    classes: PropTypes.object.isRequired,
-    allTables: PropTypes.array
+  classes: PropTypes.object.isRequired,
+  allTables: PropTypes.array
 };
 
-
 export default withStyles(styles)(SimpleTables);
-
-
