24c44daad8042d2deb7c685c23d475dbec2b386d
Jody Clements <jody@neomorphic.co.uk>
2018-10-15 10:51:15 -0400

    Removes badge from NeuronHelp.
    The badge component raises itself out of the document by setting a
    z-index of 1. This causes it to display over pop up menus, etc. Removing
    the badge doesn't seem to hurt the placement of the help icon or the
    tool tip when positioned with absolute style.

--- src/js/components/NeuronHelp.react.js
+++ src/js/components/NeuronHelp.react.js
@@ -2,57 +2,50 @@
  * Debug pop-up for neuron text fields.
 */
 
-"use strict";
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Tooltip from '@material-ui/core/Tooltip';
 import { withStyles } from '@material-ui/core/styles';
-import Badge from '@material-ui/core/Badge';
 import PropTypes from 'prop-types';
 
 const styles = () => ({
-    badge: {
-        right: "-10px",
-        width: "100px",
-        height: "50px",
-        top: "-10px",
-    }
+  badge: {
+    position: 'absolute',
+    top: '6px',
+    right: '-10px',
+    width: '2em',
+    height: '2em',
+  }
 });
 
 class NeuronHelp extends React.Component {
-    render() {
-        const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
 
-        var tooltip = (
-            <Tooltip id="tooltip-icon"
-                    title={this.props.text}
-                    placement="top"
-            >
-                <Typography color="error"
-                            variant="body1"
-                >?</Typography>
-            </Tooltip>
-        );
-        return (
-            <Badge classes={{badge: classes.badge}}
-                    color="default"
-                    badgeContent={tooltip}
-            >
-                {this.props.children}
-            </Badge>
-        );
-    }
+    var tooltip = (
+      <Tooltip id="tooltip-icon" title={this.props.text} placement="top">
+        <Typography color="error" variant="body1" className={classes.badge} >
+          ?
+        </Typography>
+      </Tooltip>
+    );
+    return (
+      <div>
+        {this.props.children}
+        {tooltip}
+      </div>
+    );
+  }
 }
 
 NeuronHelp.propTypes = {
-    children: PropTypes.element.isRequired,
-    classes: PropTypes.object.isRequired,
-    text: PropTypes.string.isRequired,
-}
-
-NeuronHelp.defaultProps = {
-    text: "Enter body ID, neuron name, or wildcard names using period+star (e.g., MBON.*)"
+  children: PropTypes.element.isRequired,
+  classes: PropTypes.object.isRequired,
+  text: PropTypes.string.isRequired
 };
 
+NeuronHelp.defaultProps = {
+  text: 'Enter body ID, neuron name, or wildcard names using period+star (e.g., MBON.*)'
+};
 
 export default withStyles(styles)(NeuronHelp);
