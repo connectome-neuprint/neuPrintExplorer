d40b8ac8a95f448ab3f99b88076adb4953a90474
Jody Clements <jody@neomorphic.co.uk>
2018-10-19 17:56:33 -0400

    Fixes broken webpack config & adds version.
    This fixes a broekn webpack configuration that was trying to include the
    package version into the site. Now fixed and you can reference the
    version of the code in the application by calling VERSION.

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const getClientEnvironment = require('./env');
-
-const publicUrl = '';
-// Get environment variables to inject into our app.
-const env = getClientEnvironment(publicUrl);
-
 
 const htmlPlugin = new HtmlWebPackPlugin({
     template: "./src/index.html",
@@ -34,7 +28,9 @@ module.exports = {
         cleanPlugin,
         miniCssExtractPlugin,
         copyWebpackPlugin,
-        new webpack.DefinePlugin(env.stringified),
+        new webpack.DefinePlugin({
+          VERSION: JSON.stringify(require('./package.json').version)
+        }),
     ],
     output: {
         path: path.resolve(__dirname, 'build'),
