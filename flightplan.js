b5ff177154ebe0b8ca71e346cbc1d65a8895a39c
Jody Clements <jody@neomorphic.co.uk>
2018-10-22 17:47:22 -0400

    Adds flightplan for automated deployment to emdata1
    Once everything is installed from npm and you have added your ssh key
    to the deploy users' authorized keys file on emdata1, you can execute
    
    fly deploy:production
    
    Then sit back and watch all the files get built and transferred
    automatically.

---
+++ flightplan.js
@@ -0,0 +1,109 @@
+var plan = require('flightplan');
+
+var config = {
+  projectDir: '/opt/www/neuprintexplorer',  // location on the remote server
+  keepReleases: 3
+};
+
+plan.target('production', {
+  host: 'emdata1',
+  username: 'deploy',
+  agent: process.env.SSH_AUTH_SOCK
+},
+{
+  // Shouldn't be overridden, so please don't try.
+  gitCheck: true
+});
+
+// Check if there are files that have not been committed to git. This stops
+// us from deploying code in an inconsistent state. It also prevents slapdash
+// changes from being deployed without a log of who added them in github. Not
+// fool proof, but better than nothing.
+plan.local('deploy', function(local) {
+  if (plan.runtime.options.gitCheck) {
+    local.log('checking git status...');
+    var result = local.exec('git status --porcelain', {silent: true});
+
+    if (result.stdout) {
+      local.log(result.stdout);
+      plan.abort('Uncommited files found, see list above');
+    }
+  } else {
+    local.log('skipping git check!!!');
+  }
+});
+
+
+plan.remote('deploy', function(remote) {
+  config.deployTo = config.projectDir + '/releases/' + (new Date().getTime());
+  remote.log('Creating webroot');
+  remote.exec('mkdir -p ' + config.deployTo);
+});
+
+plan.local('deploy', function (local) {
+  local.log('Building the production code');
+  local.exec('npm run build');
+});
+
+// grab a list of files in the build directory and copy it.
+plan.local('deploy', function (local) {
+  local.with('cd build', () => {
+    local.log('Transferring website files');
+    var files = local.exec('find . -type f -print0', {silent: true});
+    local.transfer(files, config.deployTo + '/');
+  });
+});
+
+plan.remote('deploy',function (remote) {
+  remote.log('Linking to new release');
+  remote.exec('ln -nfs ' + config.deployTo + ' ' +
+    config.projectDir + '/current');
+
+  remote.log('Checking for stale releases');
+  var releases = getReleases(remote);
+
+  if (releases.length > config.keepReleases) {
+    var removeCount = releases.length - config.keepReleases;
+    remote.log('Removing ' + removeCount + ' stale release(s)');
+
+    releases = releases.slice(0, removeCount);
+    releases = releases.map(function (item) {
+      return config.projectDir + '/releases/' + item;
+      });
+
+    remote.exec('rm -rf ' + releases.join(' '));
+  }
+});
+
+plan.remote('rollback', function(remote) {
+  remote.log('Rolling back release');
+  var releases = getReleases(remote);
+  if (releases.length > 1) {
+    var oldCurrent = releases.pop();
+    var newCurrent = releases.pop();
+    remote.log('Linking current to ' + newCurrent);
+    remote.exec('ln -nfs ' + config.projectDir + '/releases/' + newCurrent + ' '
+      + config.projectDir + '/current');
+
+    remote.log('Removing ' + oldCurrent);
+    remote.exec('rm -rf ' + config.projectDir + '/releases/' + oldCurrent);
+  }
+
+});
+
+plan.remote(['default','uptime'], function(remote) {
+  remote.exec('uptime');
+  remote.exec('whoami');
+});
+
+function getReleases(remote) {
+  var releases = remote.exec('ls ' + config.projectDir +
+    '/releases', {silent: true});
+
+  if (releases.code === 0) {
+    releases = releases.stdout.trim().split('\n');
+    return releases;
+  }
+
+  return [];
+}
