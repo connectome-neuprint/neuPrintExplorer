fa91ba151adb2692b26f69e9b377c2a268d92857
Jody Clements <jody@neomorphic.co.uk>
2018-10-26 15:42:14 -0400

    Adds Sorting to the SimpleTable view plugin.

--- src/js/components/view-plugins/SimpleTable.jsx
+++ src/js/components/view-plugins/SimpleTable.jsx
@@ -5,14 +5,51 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TablePagination from '@material-ui/core/TablePagination';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import { withStyles } from '@material-ui/core/styles';
 
 import TablePaginationActions from '../../helpers/TablePaginationActions';
 
+function desc(a, b, orderBy) {
+  let aVal = a[orderBy];
+  let bVal = b[orderBy];
+
+  // need to check if the cell has a value / action object
+  if (aVal && typeof aVal === 'object' && 'value' in aVal) {
+    aVal = aVal.value;
+  }
+
+  if (bVal && typeof bVal === 'object' && 'value' in bVal) {
+    bVal = bVal.value;
+  }
+
+  if (bVal < aVal) {
+    return -1;
+  }
+  if (bVal > aVal) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+
 const styles = theme => ({
   root: {},
   clickable: {
-    cursor: 'pointer',
+    cursor: 'pointer'
   },
   scroll: {
     width: '100%',
@@ -25,6 +62,9 @@ const styles = theme => ({
 
 class SimpleTable extends React.Component {
   state = {
+    order: 'asc',
+    orderBy: '',
+    selected: [],
     data: [],
     page: 0,
     rowsPerPage: 5
@@ -42,9 +82,20 @@ class SimpleTable extends React.Component {
     action();
   };
 
+  handleRequestSort = property => event => {
+    const orderBy = property;
+    let order = 'desc';
+
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    this.setState({ order, orderBy });
+  };
+
   render() {
     const { query, classes } = this.props;
-    const { page, rowsPerPage } = this.state;
+    const { page, rowsPerPage, orderBy, order } = this.state;
     const emptyRows =
       rowsPerPage - Math.min(rowsPerPage, query.result.data.length - page * rowsPerPage);
 
@@ -55,12 +106,22 @@ class SimpleTable extends React.Component {
             <TableHead>
               <TableRow>
                 {query.result.columns.map((header, index) => {
-                  return <TableCell key={index}>{header}</TableCell>;
+                  return (
+                    <TableCell key={index} sortDirection={orderBy === index ? order : false}>
+                      <TableSortLabel
+                        active={orderBy === index}
+                        direction={order}
+                        onClick={this.handleRequestSort(index)}
+                      >
+                        {header}
+                      </TableSortLabel>
+                    </TableCell>
+                  );
                 })}
               </TableRow>
             </TableHead>
             <TableBody>
-              {query.result.data
+              {stableSort(query.result.data, getSorting(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
                   return (
@@ -68,7 +129,15 @@ class SimpleTable extends React.Component {
                       {row.map((cell, i) => {
                         if (cell && typeof cell === 'object' && 'value' in cell) {
                           if ('action' in cell) {
-                            return <TableCell className={classes.clickable} key={i} onClick={this.handleCellClick(cell.action)}>{cell.value}</TableCell>;
+                            return (
+                              <TableCell
+                                className={classes.clickable}
+                                key={i}
+                                onClick={this.handleCellClick(cell.action)}
+                              >
+                                {cell.value}
+                              </TableCell>
+                            );
                           }
                           return <TableCell key={i}>{cell.value}</TableCell>;
                         }
