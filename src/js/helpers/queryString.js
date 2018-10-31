4cd55b40d9fa0f22614b55935ef033edeca5eb53
Jody Clements <jody@neomorphic.co.uk>
2018-10-31 16:26:23 -0400

    removed console.log() statement

--- src/js/helpers/queryString.js
+++ src/js/helpers/queryString.js
@@ -13,7 +13,6 @@ export function getQueryObject() {
 export function setQueryString(newData) {
   const currentQuery = qs.parse(window.location.search.substring(1));
   const updatedData = merge(currentQuery, newData);
-  console.log(updatedData);
   const updatedQuery = qs.stringify(updatedData);
   history.push({
     pathname: window.location.pathname,
