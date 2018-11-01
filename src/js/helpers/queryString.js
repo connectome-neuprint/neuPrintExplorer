1b6affc8bec045241f7b922899a4b42fff7cfb77
Jody Clements <jody@neomorphic.co.uk>
2018-11-01 12:02:02 -0400

    Restores neuron filter parameters to the url.
    These were not being set in the url query string after one of the code
    refactor changes. This places them back in the query string, using the
    new get/setQueryString functions.

--- src/js/helpers/queryString.js
+++ src/js/helpers/queryString.js
@@ -2,16 +2,28 @@ import qs from 'qs';
 import history from '../history';
 import merge from 'deepmerge';
 
+const keywords = {
+  true: true,
+  false: false,
+};
+
 export function getQueryString() {
   return window.location.search.substring(1);
 }
 
 export function getQueryObject() {
-  return qs.parse(getQueryString()) || {};
+  return qs.parse(decodeURIComponent(getQueryString()), {
+    decoder: function(value) {
+      if (value in keywords) {
+        return keywords[value];
+      }
+      return value;
+    }
+  }) || {};
 }
 
 export function setQueryString(newData) {
-  const currentQuery = qs.parse(window.location.search.substring(1));
+  const currentQuery = qs.parse(window.location.search.substring(1), {});
   const updatedData = merge(currentQuery, newData);
   const updatedQuery = qs.stringify(updatedData);
   history.push({
