80b2bf55fece3b4332b81d6096d808857e5de863
Jody Clements <jody@neomorphic.co.uk>
2018-11-02 09:18:30 -0400

    deepmerge module array merging changed.
    For the query string, we pass in a new array of objects and expect them
    to overwrite the existing array. This wasn't happening and the array was
    never clearing when an empty one was passed in. Adding the custom
    arrayMerge option makes it behave as expected.

--- src/js/helpers/queryString.js
+++ src/js/helpers/queryString.js
@@ -2,9 +2,10 @@ import qs from 'qs';
 import history from '../history';
 import merge from 'deepmerge';
 
+const overwriteMerge = (destinationArray, sourceArray, options) => sourceArray;
 const keywords = {
   true: true,
-  false: false,
+  false: false
 };
 
 export function getQueryString() {
@@ -12,22 +13,31 @@ export function getQueryString() {
 }
 
 export function getQueryObject() {
-  return qs.parse(decodeURIComponent(getQueryString()), {
+  return (
+    qs.parse(decodeURIComponent(getQueryString()), {
+      decoder: function(value) {
+        if (value in keywords) {
+          return keywords[value];
+        }
+        return value;
+      }
+    }) || {}
+  );
+}
+
+export function setQueryString(newData) {
+  const currentQuery = qs.parse(decodeURIComponent(getQueryString()), {
     decoder: function(value) {
       if (value in keywords) {
         return keywords[value];
       }
       return value;
     }
-  }) || {};
-}
-
-export function setQueryString(newData) {
-  const currentQuery = qs.parse(window.location.search.substring(1), {});
-  const updatedData = merge(currentQuery, newData);
+  });
+  const updatedData = merge(currentQuery, newData, { arrayMerge: overwriteMerge });
   const updatedQuery = qs.stringify(updatedData);
   history.push({
     pathname: window.location.pathname,
-    search: updatedQuery,
+    search: updatedQuery
   });
 }
