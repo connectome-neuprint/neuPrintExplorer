96d677feb02f9b9fa963d35a64716cf5e0908e40
Jody Clements <jody@neomorphic.co.uk>
2018-11-14 16:12:00 -0500

    Converts Completeness plugin to new system.

--- src/js/components/plugins/Completeness.react.js
+++ src/js/components/plugins/Completeness.react.js
@@ -2,12 +2,20 @@
  * Queries completeness of reconstruction with respect to neuron filters.
 */
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import NeuronFilter from '../NeuronFilter.react';
-import queryCompleteness from '../../neo4jqueries/completeness';
+import randomColor from 'randomcolor';
+import { withRouter } from 'react-router';
 
-export default class Completeness extends React.Component {
+import Button from '@material-ui/core/Button';
+
+import { submit } from 'actions/plugins';
+import NeuronFilter from '../NeuronFilter.react';
+import { getQueryString } from 'helpers/queryString';
+
+const pluginName = 'Completeness';
+
+class Completeness extends React.Component {
   static get queryName() {
     return 'Completeness';
   }
@@ -29,22 +37,65 @@ export default class Completeness extends React.Component {
     this.setState({ limitBig: params.limitBig, statusFilters: params.statusFilters });
   };
 
+  processResults = (query, apiResponse) => {
+    const data = apiResponse.data.map(row => {
+      return [
+        row[0], // roiname
+        ((row[1]/row[3])*100), // % pre
+        row[3], // total pre
+        ((row[2]/row[4])*100), // % post
+        row[4]  // total post
+      ];
+    });
+
+    return {
+      columns: ['ROI', '%presyn', 'total presyn', '%postsyn', 'total postsyn'],
+      data,
+      debug: apiResponse.debug
+    };
+  };
+
+  processRequest = () => {
+    const { dataSet, actions, history } = this.props;
+    const { limitBig, statusFilters } = this.state;
+
+    const parameters = {
+      dataset: dataSet,
+      statues: statusFilters,
+    };
+
+    if (limitBig) {
+      parameters.pre_threshold = 2;
+    }
+
+    const query = {
+      dataSet,
+      queryString: '/npexplorer/completeness',
+      visType: 'SimpleTable',
+      plugin: pluginName,
+      parameters,
+      title: `Coverage percentage of filtered neurons in ${dataSet}`,
+      menuColor: randomColor({ luminosity: 'light', hue: 'random' }),
+      processResults: this.processResults
+    };
+    actions.submit(query);
+    history.push({
+      pathname: '/results',
+      search: getQueryString()
+    });
+    return query;
+  };
+
   render() {
+    const { isQuerying } = this.props;
     return (
       <div>
         <NeuronFilter callback={this.loadNeuronFilters} datasetstr={this.props.datasetstr} />
         <Button
+          disabled={isQuerying}
+          color="primary"
           variant="contained"
-          onClick={() => {
-            this.props.callback(
-              queryCompleteness(
-                this.props.datasetstr,
-                this.props.availableROIs,
-                this.state.limitBig,
-                this.state.statusFilters
-              )
-            );
-          }}
+          onClick={this.processRequest}
         >
           Submit
         </Button>
@@ -58,3 +109,24 @@ Completeness.propTypes = {
   datasetstr: PropTypes.string.isRequired,
   availableROIs: PropTypes.array.isRequired
 };
+
+const CompletenessState = function(state) {
+  return {
+    isQuerying: state.query.isQuerying
+  };
+};
+
+const CompletenessDispatch = dispatch => ({
+  actions: {
+    submit: query => {
+      dispatch(submit(query));
+    }
+  }
+});
+
+export default withRouter(
+  connect(
+    CompletenessState,
+    CompletenessDispatch
+  )(Completeness)
+);
