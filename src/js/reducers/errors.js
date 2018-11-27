3da08103b279b662adf2f452495cf654b0bf5db5
neubarthn <nicoleneubarth@gmail.com>
2018-11-27 16:16:40 -0500

    added check for existence of action type in errors reducer

--- src/js/reducers/errors.js
+++ src/js/reducers/errors.js
@@ -1,17 +1,18 @@
 import Immutable from 'immutable';
 
-var errorsState = Immutable.Map({
-  message: null,
+const errorsState = Immutable.Map({
+  message: null
 });
 
 export default function errorsReducer(state = errorsState, action) {
-  if (action.type.match(/ERROR$/)) {
-    if (typeof(action.error) === 'string') {
+  if (action.type && action.type.match(/ERROR$/)) {
+    if (typeof action.error === 'string') {
       return state.set('message', action.error);
     }
     return state.set('message', action.error.message);
-  } else if (action.type.match('CLEAR_ERRORS')) {
+  } else if (action.type && action.type.match('CLEAR_ERRORS')) {
     return errorsState;
+  } else {
+    return state;
   }
-  return state;
 }
