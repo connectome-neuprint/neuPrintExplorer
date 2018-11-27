504dd2feda1243833b6bca344437827a9d285a17
neubarthn <nicoleneubarth@gmail.com>
2018-11-27 16:21:38 -0500

    refactor neo4jsettings reducer

--- src/js/reducers/neo4jsettings.js
+++ src/js/reducers/neo4jsettings.js
@@ -1,29 +1,29 @@
 /*
  * Stores configuration and other basic information for neo4j datasets.
 */
+import C from './constants';
+import Immutable from 'immutable';
 
-"";
-
-import C from "./constants"
-
-var neo4jsettingsState = {
-    availableDatasets: [],
-    availableROIs: {},
-    datasetInfo: {},
-    neoServer: "",
-}
-
+var neo4jsettingsState = Immutable.Map({
+  availableDatasets: [],
+  availableROIs: {},
+  datasetInfo: {},
+  neoServer: ''
+});
 
 export default function neo4jreducer(state = neo4jsettingsState, action) {
-    switch (action.type) {
-        case C.SET_NEO_DATASETS: {
-            return Object.assign({}, state, { availableDatasets: action.availableDatasets, availableROIs: action.availableROIs, datasetInfo: action.datasetInfo });
-        }
-        case C.SET_NEO_SERVER: {
-            return Object.assign({}, state, { neoServer: action.neoServer });
-        }
-        default: {
-            return state;
-        }
+  switch (action.type) {
+    case C.SET_NEO_DATASETS: {
+      return state
+        .set('availableDatasets', action.availableDatasets)
+        .set('availableROIs', action.availableROIs)
+        .set('datasetInfo', action.datasetInfo);
     }
+    case C.SET_NEO_SERVER: {
+      return state.set(('neoServer', action.neoServer));
+    }
+    default: {
+      return state;
+    }
+  }
 }
--- src/js/reducers/neo4jsettings.test.js
+++ src/js/reducers/neo4jsettings.test.js
@@ -1,47 +1,49 @@
-import C from "../../../src/js/reducers/constants"
-import reducer from "../../../src/js/reducers/neo4jsettings"
+import C from '../../../src/js/reducers/constants';
+import reducer from '../../../src/js/reducers/neo4jsettings';
+import Immutable from 'immutable';
 
-const state = {
-    availableDatasets: ["existingdataset"],
-    availableROIs: { rois: ["existingrois"] },
-    datasetInfo: { 
-        lastmod: "existinglastmod",
-        uuid: "existingversion"
-    },
-    neoServer: "foobar"
-}
+const state = Immutable.Map({
+  availableDatasets: ['existingdataset'],
+  availableROIs: { rois: ['existingrois'] },
+  datasetInfo: {
+    lastmod: 'existinglastmod',
+    uuid: 'existingversion'
+  },
+  neoServer: 'foobar'
+});
 
-describe("neo4jsettings Reducer", () => {
-    it("SET_NEO_DATASETS success", () => {
-        const action = {
-            type: C.SET_NEO_DATASETS,
-            availableDatasets: ["newdataset"],
-            availableROIs: { rois: ["newrois"] },
-            datasetInfo: { 
-                lastmod: "newlastmod",
-                uuid: "newversion"
-            }
-        };
-        expect((reducer(undefined, action)))
-            .toEqual({
-                availableDatasets: ["newdataset"],
-                availableROIs: { rois: ["newrois"] },
-                datasetInfo: { 
-                    lastmod: "newlastmod",
-                    uuid: "newversion"
-                },  
-                neoServer: ""
-            });
-        expect((reducer(state, action)))
-            .toEqual({
-                availableDatasets: ["newdataset"],
-                availableROIs: { rois: ["newrois"] },
-                datasetInfo: { 
-                    lastmod: "newlastmod",
-                    uuid: "newversion"
-                },
-                neoServer: "foobar"
-            });
-    })
-})
-
+describe('neo4jsettings Reducer', () => {
+  it('SET_NEO_DATASETS success', () => {
+    const action = {
+      type: C.SET_NEO_DATASETS,
+      availableDatasets: ['newdataset'],
+      availableROIs: { rois: ['newrois'] },
+      datasetInfo: {
+        lastmod: 'newlastmod',
+        uuid: 'newversion'
+      }
+    };
+    expect(reducer(undefined, action)).toEqual(
+      Immutable.Map({
+        availableDatasets: ['newdataset'],
+        availableROIs: { rois: ['newrois'] },
+        datasetInfo: {
+          lastmod: 'newlastmod',
+          uuid: 'newversion'
+        },
+        neoServer: ''
+      })
+    );
+    expect(reducer(state, action)).toEqual(
+      Immutable.Map({
+        availableDatasets: ['newdataset'],
+        availableROIs: { rois: ['newrois'] },
+        datasetInfo: {
+          lastmod: 'newlastmod',
+          uuid: 'newversion'
+        },
+        neoServer: 'foobar'
+      })
+    );
+  });
+});
