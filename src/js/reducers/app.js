1c06991ab95e95ed90473578d0f1b3ddc76afb54
neubarthn <nicoleneubarth@gmail.com>
2018-11-27 16:14:47 -0500

    refactor app.js reducer

--- src/js/reducers/app.js
+++ src/js/reducers/app.js
@@ -1,49 +1,47 @@
 /*
  * Store high-level app state.
 */
-import C from "./constants";
+import C from './constants';
 import Immutable from 'immutable';
 
 const appState = Immutable.Map({
-    pluginList: Immutable.List([]),
-    reconIndex: 9999999,
-    urlQueryString: window.location.search.substring(1),
-    appDB: "",
-    fullscreen: false,
-    activePlugins: Immutable.Map({}),
-    viewPlugins: Immutable.Map({}),
+  pluginList: Immutable.List([]),
+  reconIndex: 9999999,
+  urlQueryString: window.location.search.substring(1),
+  appDB: '',
+  fullscreen: false,
+  activePlugins: Immutable.Map({}),
+  viewPlugins: Immutable.Map({})
 });
 
 export default function appReducer(state = appState, action) {
-    if (state === undefined) {
-        return appState;
+  switch (action.type) {
+    case C.INIT_PLUGINS:
+      return state
+        .set('pluginList', Immutable.List(action.pluginList))
+        .set('reconIndex', action.reconIndex);
+    case C.INIT_VIEWPLUGINS:
+      return state.set('viewPlugins', Immutable.Map(action.plugins));
+    case C.SET_URL_QS:
+      return state.set('urlQueryString', action.urlQueryString);
+    case C.ACTIVATE_PLUGIN: {
+      const id = action.uuid;
+      const dataElement = Immutable.Map({
+        data: action.data,
+        query: action.query,
+        viz: action.viz
+      });
+      return state.setIn(['activePlugins', id], dataElement);
     }
-
-    switch (action.type) {
-        case C.INIT_PLUGINS:
-            return state.set("pluginList", action.pluginList).set("reconIndex", action.reconIndex);
-        case C.INIT_VIEWPLUGINS:
-            return state.set("viewPlugins", Immutable.Map(action.plugins));
-        case C.SET_URL_QS:
-            return state.set("urlQueryString", action.urlQueryString);
-        case C.ACTIVATE_PLUGIN: {
-            const id = action.uuid;
-            const dataElement = Immutable.Map({
-                data: action.data,
-                query: action.query,
-                viz: action.viz,
-            });
-            return state.setIn(["activePlugins", id], dataElement);
-        }
-        case C.DEACTIVATE_PLUGIN:
-            return state.deleteIn(["activePlugins", action.uuid]);
-        case C.SET_APP_DB:
-            return state.set("appDB", action.appDB);
-        case C.SET_FULLSCREEN_VIEWER:
-            return state.set('fullscreen', true);
-        case C.CLEAR_FULLSCREEN_VIEWER:
-            return state.set('fullscreen', false);
-        default:
-            return state;
-    }
+    case C.DEACTIVATE_PLUGIN:
+      return state.deleteIn(['activePlugins', action.uuid]);
+    case C.SET_APP_DB:
+      return state.set('appDB', action.appDB);
+    case C.SET_FULLSCREEN_VIEWER:
+      return state.set('fullscreen', true);
+    case C.CLEAR_FULLSCREEN_VIEWER:
+      return state.set('fullscreen', false);
+    default:
+      return state;
+  }
 }
