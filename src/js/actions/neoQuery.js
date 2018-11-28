be92361e9a74e77410e5b22df66a889d1c71c2a5
neubarthn <nicoleneubarth@gmail.com>
2018-11-28 11:30:54 -0500

    prettier reformat

--- src/js/actions/neoQuery.js
+++ src/js/actions/neoQuery.js
@@ -1,29 +1,28 @@
 import C from '../reducers/constants';
 
-
 export function setQueryError(error) {
   return {
     type: C.SET_NEO_ERROR,
-    neoError: error,
+    neoError: error
   };
 }
 
 export function appendData(results) {
   return {
     type: C.APPEND_RESULTS,
-    allTables: results,
+    allTables: results
   };
 }
 
 export function saveData(results) {
   return {
     type: C.UPDATE_RESULTS,
-    allTables: results,
+    allTables: results
   };
 }
 
 export function finishQuery() {
   return {
-    type: C.FINISH_QUERY,
+    type: C.FINISH_QUERY
   };
 }
