a8c9eaeeeac6e3433a95ea4248469c33d6d3e145
neubarthn <nicoleneubarth@gmail.com>
2018-11-28 11:00:24 -0500

    user reducer uses Immutable.js

--- src/js/reducers/user.js
+++ src/js/reducers/user.js
@@ -2,26 +2,27 @@
  * Store user state / login.
 */
 
-import C from "./constants"
+import C from './constants';
+import Immutable from 'immutable';
 
-var userState = {
-    userInfo: null,
-    token: "",
-}
+var userState = Immutable.Map({
+  userInfo: null,
+  token: ''
+});
 
 export default function userReducer(state = userState, action) {
-    switch (action.type) {
-        case C.LOGIN_USER: {
-            return Object.assign({}, state, { userInfo: action.userInfo });
-        }
-        case C.LOGOUT_USER: {
-            return Object.assign({}, state, { userInfo: null, token: "" });
-        }
-        case C.SET_USER_TOKEN: {
-            return Object.assign({}, state, { token: action.token });
-        }
-        default: {
-            return state;
-        }
+  switch (action.type) {
+    case C.LOGIN_USER: {
+      return state.set('userInfo', action.userInfo);
     }
+    case C.LOGOUT_USER: {
+      return state.set('userInfo', null).set('token', '');
+    }
+    case C.SET_USER_TOKEN: {
+      return state.set('token', action.token);
+    }
+    default: {
+      return state;
+    }
+  }
 }
--- src/js/reducers/user.test.js
+++ src/js/reducers/user.test.js
@@ -1,44 +1,32 @@
-import C from "../../../src/js/reducers/constants"
-import reducer from "../../../src/js/reducers/user"
+import C from '../../../src/js/reducers/constants';
+import reducer from '../../../src/js/reducers/user';
+import Immutable from 'immutable';
 
-const state = {
-    userInfo: "existinguserinfo",
-    token: "existingxyz"
-}
+const state = Immutable.Map({
+  userInfo: 'existinguserinfo',
+  token: 'existingxyz'
+});
 
-describe("user Reducer", () => {
+const initialState = Immutable.Map({
+  userInfo: null,
+  token: ''
+});
 
-    it("LOGIN_USER success", () => {
-        const action = {
-            type: C.LOGIN_USER,
-            userInfo: "newuserinfo"
-        };
-        expect(reducer(undefined,action))
-        .toEqual({
-            userInfo: "newuserinfo",
-            token: ""
-        });
-        expect(reducer(state,action))
-        .toEqual({
-            userInfo: "newuserinfo",
-            token: "existingxyz"
-        });
-    })
+describe('user Reducer', () => {
+  it('LOGIN_USER success', () => {
+    const action = {
+      type: C.LOGIN_USER,
+      userInfo: 'newuserinfo'
+    };
+    expect(reducer(undefined, action)).toEqual(initialState.set('userInfo', 'newuserinfo'));
+    expect(reducer(state, action)).toEqual(state.set('userInfo', 'newuserinfo'));
+  });
 
-    it("LOGOUT_USER success", () => {
-        const action = {
-            type: C.LOGOUT_USER
-        };
-        expect(reducer(undefined,action))
-        .toEqual({
-            userInfo: null,
-            token: ""
-        });
-        expect(reducer(state,action))
-        .toEqual({
-            userInfo: null,
-            token: ""
-        });
-    })
-
-})
+  it('LOGOUT_USER success', () => {
+    const action = {
+      type: C.LOGOUT_USER
+    };
+    expect(reducer(undefined, action)).toEqual(initialState);
+    expect(reducer(state, action)).toEqual(initialState);
+  });
+});
