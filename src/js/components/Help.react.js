c665a2363e3a4097019a20e16ae79871ef640f0f
neubarthn <nicoleneubarth@gmail.com>
2018-12-04 15:27:27 -0500

    graph model image references png posted on neuprint github

--- src/js/components/Help.react.js
+++ src/js/components/Help.react.js
@@ -3,15 +3,15 @@
 */
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import _ from "underscore";
+import _ from 'underscore';
 import PropTypes from 'prop-types';
-import {withStyles} from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Modal from '@material-ui/core/Modal';
 //import { Deck, Slide, Image } from 'spectacle';
 //import {SwaggerUI} from 'react-swagger-ui'
 //import 'react-swagger-ui/dist/swagger-ui.css'
-import SwaggerUi, {presets} from 'swagger-ui';
+import SwaggerUi, { presets } from 'swagger-ui';
 import 'swagger-ui/dist/swagger-ui.css';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
@@ -19,169 +19,162 @@ import AppBar from '@material-ui/core/AppBar';
 
 function TabContainer(props) {
   return (
-    <Typography component="div" 
-                style={{ padding: 8 * 3 }}
-    >
+    <Typography component="div" style={{ padding: 8 * 3 }}>
       {props.children}
     </Typography>
   );
 }
 
 TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 };
 
 const styles = theme => ({
-    root: {
-        overflow: "scroll",
-        position: "relative",
-        width: "100%",
-        height: "100%",
-        padding: theme.spacing.unit * 3,
-    },
-    roottext: {
-        flex: 1
-    },
-    flex: {
-        flex: 1,
-        padding: '1em'
-    },
-    overflow: {
-        overflow: "auto"
-    },
-    secroot: {
-        position: "relative",
-        width: "100%",
-        height: "100%",
-    },
-    img: {
-        maxWidth: "100%",
-        minWidth: "500px",
-    },
+  root: {
+    overflow: 'scroll',
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+    padding: theme.spacing.unit * 3
+  },
+  roottext: {
+    flex: 1
+  },
+  flex: {
+    flex: 1,
+    padding: '1em'
+  },
+  overflow: {
+    overflow: 'auto'
+  },
+  secroot: {
+    position: 'relative',
+    width: '100%',
+    height: '100%'
+  },
+  img: {
+    maxWidth: '100%',
+    minWidth: '500px'
+  }
 });
 
 class Help extends React.Component {
-    constructor(props) {
-       super(props);
-       this.state = {
-           open: false,
-           value: 0,
-       }
-    }
-   
-    // if only query string has updated, prevent re-render
-    shouldComponentUpdate(nextProps, nextState) {
-        nextProps.location["search"] = this.props.location["search"];
-        return (!_.isEqual(nextProps, this.props) || !_.isEqual(nextState, this.state));
-    }
- 
-    componentDidUpdate() {
-        if (this.state.value === 1) {
-            SwaggerUi({
-                dom_id: '#swaggerContainer',
-                url: '/api/help/swagger.yaml',
-                presets: [presets.apis],
-            });
-        }
-    }
-
-    handleOpen = () => {
-        this.setState({open: true});
-    }
-
-    handleClose = () => {
-        this.setState({open: false});
-    }
-
-    handleChange = (event, value) => {
-        this.setState({ value });
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      value: 0
     };
+  }
 
-    render() {
-        const {classes} = this.props;
-        const { value } = this.state;
+  // if only query string has updated, prevent re-render
+  shouldComponentUpdate(nextProps, nextState) {
+    nextProps.location['search'] = this.props.location['search'];
+    return !_.isEqual(nextProps, this.props) || !_.isEqual(nextState, this.state);
+  }
 
-                //<SwaggerUI url='/swagger.yaml' spec={object} />
-        return (
-            <div className={classes.root}>
-                <AppBar 
-                        position="static"
-                        color="default"
-                >
-                    <Tabs value={value} 
-                          onChange={this.handleChange}
-                    >
-                        <Tab label="Model Overview" />
-                        <Tab label="neuPrint API" />
-                        <Tab label="Slides" />
-                    </Tabs>
-                </AppBar>
-                
-                {value === 0 && <TabContainer>
-                     <div className={classes.roottext}>
-                        <Typography variant="h6">Graph Data Model</Typography>
-                        <Typography >
-                            The data is stored in Neo4j, a graph database, and is accessible
-                            through custom Cypher queries.  This app provides a series
-                            of interfaces to simplify common access patterns.
-                        </Typography>
-                        <br />
-                        <Typography >
-                            The primary entry point into the graph model is the neuron 
-                            node type.  Neurons (each which could be a subset or superset of an actual
-                            neuron due to errors in automatic image segmentation) are connected
-                            to other neurons via synapse connections.  To provide more granularity
-                            into the connectomic dataset, these nodes point to a set of synapses
-                            that give the exact locations of all synaptic connections for a given
-                            neuron.  In a similar way, the morphology of the neuron is encoded
-                            by a link to set of skeleton nodes with size and shape values.  Each
-                            neuron and synapse is labeled with the given region(s) that they
-                            belong to for fast region-based queries.  Finally, the probability
-                            that two neurons should be merged (due to oversegmentation errors) 
-                            is encoded by a relationship between nodes.
-                        </Typography>
-                        <br />
-                        <Typography >
-                            Specific properties can be easily added to Neo4j as needed.  More
-                            details of the graph model are shown in the accompanying illustration.
-                        </Typography>
-                    </div>
-                    <div className={classes.flex}>
-                        <Button onClick={this.handleOpen}>
-                        <img
-                            src="/public/overview.png"
-                            alt="Neo4j graph model diagram"
-                            className={classes.img}
-                        />
-                        </Button>
-                        <Modal
-                                aria-labelledby="Neo4j Graph Model"
-                                aria-describedby="simple-modal-description"
-                                open={this.state.open}
-                                onClose={this.handleClose}
-                        >
-                            <div className={classes.overflow}>
-                            <Button onClick={this.handleClose}>
-                                <img
-                                  src="/public/overview.png"
-                                  alt="Neo4j graph model diagram"
-                                />
-                            </Button>
-                            </div>
-                        </Modal>
-                    </div>
-                </TabContainer>
-                }
-                {value === 1 && <TabContainer>
-                    <div id="swaggerContainer" />
-                </TabContainer>}
-                {value === 2 && <TabContainer>
-                    <div className={classes.roottext}>
-                        <Typography >
-                            The following slides describe how data is stored in Neo4j.
-                        </Typography>
-                    </div>
-                    {/* <div className={classes.secroot}>
+  componentDidUpdate() {
+    if (this.state.value === 1) {
+      SwaggerUi({
+        dom_id: '#swaggerContainer',
+        url: '/api/help/swagger.yaml',
+        presets: [presets.apis]
+      });
+    }
+  }
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    //<SwaggerUI url='/swagger.yaml' spec={object} />
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Tabs value={value} onChange={this.handleChange}>
+            <Tab label="Model Overview" />
+            <Tab label="neuPrint API" />
+            <Tab label="Slides" />
+          </Tabs>
+        </AppBar>
+
+        {value === 0 && (
+          <TabContainer>
+            <div className={classes.roottext}>
+              <Typography variant="h6">Graph Data Model</Typography>
+              <Typography>
+                The data is stored in Neo4j, a graph database, and is accessible through custom
+                Cypher queries. This app provides a series of interfaces to simplify common access
+                patterns.
+              </Typography>
+              <br />
+              <Typography>
+                The primary entry point into the graph model is the neuron node type. Neurons (each
+                which could be a subset or superset of an actual neuron due to errors in automatic
+                image segmentation) are connected to other neurons via synapse connections. To
+                provide more granularity into the connectomic dataset, these nodes point to a set of
+                synapses that give the exact locations of all synaptic connections for a given
+                neuron. In a similar way, the morphology of the neuron is encoded by a link to set
+                of skeleton nodes with size and shape values. Each neuron and synapse is labeled
+                with the given region(s) that they belong to for fast region-based queries. Finally,
+                the probability that two neurons should be merged (due to oversegmentation errors)
+                is encoded by a relationship between nodes.
+              </Typography>
+              <br />
+              <Typography>
+                Specific properties can be easily added to Neo4j as needed. More details of the
+                graph model are shown in the accompanying illustration.
+              </Typography>
+            </div>
+            <div className={classes.flex}>
+              <Button onClick={this.handleOpen}>
+                <img
+                  src="https://raw.githubusercontent.com/connectome-neuprint/neuPrint/master/pgmv1.png"
+                  alt="Neo4j graph model diagram"
+                  className={classes.img}
+                />
+              </Button>
+              <Modal
+                aria-labelledby="Neo4j Graph Model"
+                aria-describedby="simple-modal-description"
+                open={this.state.open}
+                onClose={this.handleClose}
+              >
+                <div className={classes.overflow}>
+                  <Button onClick={this.handleClose}>
+                    <img
+                      src="https://raw.githubusercontent.com/connectome-neuprint/neuPrint/master/pgmv1.png"
+                      alt="Neo4j graph model diagram"
+                    />
+                  </Button>
+                </div>
+              </Modal>
+            </div>
+          </TabContainer>
+        )}
+        {value === 1 && (
+          <TabContainer>
+            <div id="swaggerContainer" />
+          </TabContainer>
+        )}
+        {value === 2 && (
+          <TabContainer>
+            <div className={classes.roottext}>
+              <Typography>The following slides describe how data is stored in Neo4j.</Typography>
+            </div>
+            {/* <div className={classes.secroot}>
                         <Deck
                                 controls
                         >
@@ -199,17 +192,18 @@ class Help extends React.Component {
                         }
                         </Deck>
                     </div> */}
-                </TabContainer>}
-            </div>
-        );
-    }
+          </TabContainer>
+        )}
+      </div>
+    );
+  }
 }
 
 Help.propTypes = {
-    location: PropTypes.shape({
-        search: PropTypes.string.isRequired
-    }),
-    classes: PropTypes.object,
-}
+  location: PropTypes.shape({
+    search: PropTypes.string.isRequired
+  }),
+  classes: PropTypes.object
+};
 
 export default withStyles(styles)(Help);
