de87d41f000dde19049d89eeea019a9b5d0f20a0
Jody Clements <jody@neomorphic.co.uk>
2018-12-06 12:02:37 -0500

    Modifying some tests.
    These tests were either no longer relevant due to changes in the
    reducers or needed to be updated to reflect the changes to the returned
    data structure.

--- src/js/reducers/results.js
+++ src/js/reducers/results.js
@@ -6,7 +6,6 @@ import Immutable from 'immutable';
 import C from './constants';
 
 const resultsState = {
-  allTables: null,
   clearIndices: new Set(),
   numClear: 0,
   allResults: Immutable.List([]),
@@ -14,30 +13,6 @@ const resultsState = {
 
 export default function resultsReducer(state = resultsState, action) {
   switch (action.type) {
-    case C.UPDATE_RESULTS: {
-      return Object.assign({}, state, {
-        allTables: [action.allTables],
-        clearIndices: new Set(),
-        numClear: 0
-      });
-    }
-    case C.APPEND_RESULTS: {
-      if (state.allTables !== null) {
-        return Object.assign({}, state, {
-          allTables: [...state.allTables.slice(0, state.allTables.size), action.allTables]
-        });
-      }
-      return Object.assign({}, state, { allTables: [action.allTables] });
-    }
-    case C.CLEAR_RESULT: {
-      if (state.allTables !== null && state.allTables.length > action.index) {
-        const indices = new Set(state.clearIndices);
-        indices.add(action.index);
-        return Object.assign({}, state, { clearIndices: indices, numClear: indices.size });
-      }
-
-      return state;
-    }
     case C.CLEAR_NEW_RESULT: {
       const updated = state.allResults.remove(action.index);
       return Object.assign({}, state, { allResults: updated});
