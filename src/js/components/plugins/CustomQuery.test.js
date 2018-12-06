99c764f86e1cb617e5ee55131605aa3abb73ecd2
neubarthn <nicoleneubarth@gmail.com>
2018-12-06 13:03:04 -0500

    completed tests for custom query plugin

--- src/js/components/plugins/CustomQuery.test.js
+++ src/js/components/plugins/CustomQuery.test.js
@@ -14,6 +14,11 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(AppReducers, {}, composeEnhancers(applyMiddleware(thunk)));
 const history = createMemoryHistory('/');
 
+let wrapper;
+let button;
+let textField;
+let submit;
+
 const component = (
   <Provider store={store}>
     <Router history={history}>
@@ -23,6 +28,19 @@ const component = (
 );
 
 describe('custom query Plugin', () => {
+  beforeAll(() => {
+    wrapper = mount(component);
+    button = wrapper.find('CustomQuery').find('Button');
+    textField = wrapper.find('CustomQuery').find('TextField');
+    submit = jest.spyOn(wrapper.find('CustomQuery').props().actions, 'submit');
+  });
+  beforeEach(() => {
+    submit.mockReset();
+  });
+  it('has name and description', () => {
+    expect(CustomQuery.queryName).toBeTruthy();
+    expect(CustomQuery.queryDescription).toBeTruthy();
+  });
   it('renders correctly', () => {
     const pluginView = renderer.create(component).toJSON();
     expect(pluginView).toMatchInlineSnapshot(`
@@ -108,17 +126,66 @@ describe('custom query Plugin', () => {
 `);
   });
   describe('when user clicks submit', () => {
-    it('should submit a request to neuprinthttp', () => {
-      const wrapper = mount(component);
-      const button = wrapper.children().find('button');
-      console.log(wrapper.debug());
+    it('should return a query object and submit', () => {
+      submit = jest.spyOn(wrapper.find('CustomQuery').props().actions, 'submit');
+      expect(button.props().onClick()).toEqual(
+        expect.objectContaining({
+          dataSet: undefined,
+          cypherQuery: '',
+          visType: 'SimpleTable',
+          plugin: 'CustomQuery',
+          parameters: {},
+          title: 'Custom query',
+          menuColor: expect.any(String),
+          processResults: expect.any(Function)
+        })
+      );
+      expect(submit).toHaveBeenCalledTimes(1);
     });
-    it('should process returned results from neuprinthttp', () => {});
+    it('should process returned results into data object', () => {
+      const query = {
+        dataSet: 'test',
+        cypherQuery: 'test',
+        visType: 'SimpleTable',
+        plugin: 'CustomQuery',
+        parameters: {},
+        title: 'Custom query'
+      };
+      const apiResponse = { data: [[1, 2, 3], [4, 5, 6]], columns: ['a', 'b', 'c'], debug: 'test' };
+      const processedResults = wrapper
+        .find('CustomQuery')
+        .instance()
+        .processResults(query, apiResponse);
+      expect(processedResults).toEqual(apiResponse);
+
+      // if no data returned
+      const processedResultsEmpty = wrapper
+        .find('CustomQuery')
+        .instance()
+        .processResults(query, {});
+      expect(processedResultsEmpty).toEqual({
+        columns: [],
+        data: [],
+        debug: ''
+      });
+    });
   });
   describe('when user hits enter key', () => {
-    it('should submit request', () => {});
+    it('should submit request', () => {
+      const processRequest = jest.spyOn(wrapper.find('CustomQuery').instance(), 'processRequest');
+      const preventDefault = jest.fn();
+      textField.props().onKeyDown({ keyCode: 13, preventDefault: preventDefault });
+      expect(preventDefault).toHaveBeenCalledTimes(1);
+      expect(processRequest).toHaveBeenCalledTimes(1);
+      expect(submit).toHaveBeenCalledTimes(1);
+    });
   });
-  describe('when user types input into text box', () => {
-    it('should save parameters to url', () => {});
+  describe('when user inputs text', () => {
+    it('should change url query string in state', () => {
+      const setUrlQs = jest.spyOn(wrapper.find('CustomQuery').props().actions, 'setURLQs');
+      textField.props().onChange({ target: { value: 'abc' } });
+      expect(wrapper.find('CustomQuery').state('qsParams').textValue).toBe('abc');
+      expect(setUrlQs).toHaveBeenCalledTimes(1);
+    });
   });
 });
