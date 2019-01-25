9fdbc8179a62b74835e11665c71ec01f5f7561cc
Jody Clements <jody@neomorphic.co.uk>
2019-01-25 14:30:51 -0500

    Make it easier to load external plugins.
    This changes the way that external collaborators can install plugins.
    Rather than having to add them to the @neuprint package, they can simply
    drop the plugin into the plugins or view-plugins directory.

--- src/js/helpers/initplugins.js
+++ src/js/helpers/initplugins.js
@@ -1,6 +1,6 @@
 /*
  * Loads plugin modules and names from plugin directory.
-*/
+ */
 
 import { initPlugins, initViewPlugins } from 'actions/app';
 
@@ -9,12 +9,29 @@ const pluginList = [];
 const plugins = require('@neuprint/queries');
 const viewPlugins = require('@neuprint/views');
 
-// const alt_view_plugins = require.context("../../../node_modules/@neuprint/views/build", false, /.js$/);
+const extQueries = require.context('../../../plugins/', true, /.jsx?$/);
+const extViews = require.context('../../../view-plugins/', true, /.jsx?$/);
 
+// load the external view plugins
+extViews.keys().forEach(key => {
+  // get plugin name from file name
+  const nameMatch = key.match(/^(?!.*\.test\.jsx?$)\.\/(.*)\.jsx?$/);
+  if (nameMatch) {
+    const pluginName = nameMatch[1];
+    viewPlugins[pluginName] = extViews(key).default;
+  }
+});
+
+// load the core query plugins
 Object.keys(plugins).forEach(key => {
   pluginList.push(plugins[key]);
 });
+// load the external query plugins
+extQueries.keys().forEach(key => {
+  pluginList.unshift(extQueries(key).default);
+});
 
+// load all the plugins into the redux store
 export default function loadPlugins(store) {
   store.dispatch(initPlugins(pluginList));
   store.dispatch(initViewPlugins(viewPlugins));
