7838942deeb35eda7741c9781f58c7c0bd717395
neubarthn <nicoleneubarth@gmail.com>
2019-03-14 13:35:19 -0400

    created a dev-public script for developing the public version of npe

---
+++ webpack.public-dev.js
@@ -0,0 +1,14 @@
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const common = require('./webpack.config.js');
+
+module.exports = merge(
+  {
+    plugins: [
+      new webpack.DefinePlugin({
+        PUBLIC: 'true'
+      })
+    ]
+  },
+  common
+);
