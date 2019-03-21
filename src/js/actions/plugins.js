d330d66ac3b967fa795e6b572148ba011cd674b9
Jody Clements <jody@neomorphic.co.uk>
2019-03-21 15:45:43 -0400

    Removes the submit action from plugins actions.
    This was the old way of submitting a query and is no longer used, so I
    have removed it to avoid confusion.

--- src/js/actions/plugins.js
+++ src/js/actions/plugins.js
@@ -1,30 +1,9 @@
 import C from '../reducers/constants';
 
-function submittingQuery(query) {
-  return {
-    type: C.PLUGIN_SUBMITTING,
-    query
-  };
-}
-
-function submissionError(error) {
-  return {
-    type: C.PLUGIN_SUBMIT_ERROR,
-    error: `PLUGIN ERROR: ${error}`
-  };
-}
-
 export function pluginResponseError(error) {
   return {
     type: C.PLUGIN_RESPONSE_ERROR,
     error: `PLUGIN RESPONSE ERROR: ${error}`
-  };
-}
-
-function saveQueryResponse(combined) {
-  return {
-    type: C.PLUGIN_SAVE_RESPONSE,
-    combined
   };
 }
 
@@ -33,59 +12,6 @@ export function updateQuery(index, newQueryObject) {
     type: C.UPDATE_QUERY,
     index,
     queryObject: newQueryObject
-  };
-}
-
-const handleError = response => {
-  if (!response.ok) {
-    if (response.status !== 400) {
-      throw new Error(`Failed to process query. [Status Code: ${response.status}]`);
-    }
-  }
-  return response;
-};
-
-export function submit(query) {
-  return function submitAsync(dispatch) {
-    dispatch(submittingQuery(query));
-
-    // build the query url. Use the custom one by default.
-    let queryUrl = '/api/custom/custom';
-    if (query.queryString) {
-      queryUrl = `/api${query.queryString}`;
-    }
-
-    const { parameters } = query;
-    // if cypherQuery is passed in, then add it to the parameters.
-    if (query.cypherQuery) {
-      parameters.cypher = query.cypherQuery;
-    }
-
-    // async action here to fetch the results and format them.
-    return fetch(queryUrl, {
-      headers: {
-        'content-type': 'application/json',
-        Accept: 'application/json'
-      },
-      body: JSON.stringify(parameters),
-      method: 'POST',
-      credentials: 'include'
-    })
-      .then(result => handleError(result))
-      .then(result => result.json())
-      .then(resp => {
-        // sends error message provided by neuprinthttp
-        if (resp.error) {
-          throw new Error(resp.error);
-        }
-        // make new result object
-        const data = query.processResults(query, resp);
-        const combined = Object.assign(query, { result: data });
-        dispatch(saveQueryResponse(combined));
-      })
-      .catch(error => {
-        dispatch(submissionError(error));
-      });
   };
 }
 
