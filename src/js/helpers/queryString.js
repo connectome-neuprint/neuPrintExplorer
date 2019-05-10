ca050614b6f95b04f2c4acd4d2e892467e5ca4ac
Jody Clements <jody@neomorphic.co.uk>
2019-05-10 11:10:11 -0400

    Removes duplicate key

--- src/js/helpers/queryString.js
+++ src/js/helpers/queryString.js
@@ -112,9 +112,9 @@ export function updateResultInQueryString(index, newData) {
 export function getSiteParams(location) {
   const decoded =
     qs.parse(decodeURIComponent(location.search.substring(1)), {
-    // that is passed to it. This limit would convert any array over a length
-    // of 20 to an object. That is bad, so bump it to 2000.
-    arrayLimit: 2000,
+      // The qs library set a limit of 20 for the number of items in an array
+      // that is passed to it. This limit would convert any array over a length
+      // of 20 to an object. That is bad, so bump it to 2000.
       arrayLimit: 2000,
       decoder: function decoder(value) {
         if (value in keywords) {
