715647fb666a433609610c2a800b2272f140fa73
Jody Clements <jody@neomorphic.co.uk>
2019-07-10 09:55:00 -0400

    Adds single click copy to clipboard for cypher.
    The cypher query text can now be copied to the clipboard with a single
    click. A notification is shown to indicate that the copy has occurred.

--- src/js/components/CypherQuery.jsx
+++ src/js/components/CypherQuery.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
-import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+
+import { launchNotification } from 'actions/app';
+import C from '../reducers/constants';
 
 const styles = {
   card: {
@@ -19,12 +25,36 @@ const styles = {
 };
 
 function CypherQuery(props) {
-  const { cypherString, classes } = props;
+  const { cypherString, classes, actions } = props;
+
+  function handleClose(e) {
+    e.preventDefault();
+    actions.closeCypher();
+  }
+
+  function handleCopy() {
+    actions.launchNotification('Text Copied to Clipboard');
+  }
+
   return (
     <Card className={classes.card}>
       <CardContent>
-        <CardHeader title="Neo4j Cypher Query" />
-        <pre className={classes.code}>{cypherString}</pre>;
+        <CardHeader
+          title="Neo4j Cypher Query"
+          action={
+            <IconButton aria-label="Close" onClick={handleClose}>
+              <CloseIcon />
+            </IconButton>
+          }
+        />
+        <pre className={classes.code}>
+          <CopyToClipboard
+            text={cypherString}
+            onCopy={handleCopy}
+          >
+            <span>{cypherString}</span>
+          </CopyToClipboard>
+        </pre>
       </CardContent>
     </Card>
   );
@@ -32,7 +62,28 @@ function CypherQuery(props) {
 
 CypherQuery.propTypes = {
   cypherString: PropTypes.string.isRequired,
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(CypherQuery);
+const CypherQueryDispatch = dispatch => ({
+  actions: {
+    closeCypher: () => {
+      dispatch({
+        type: C.TOGGLE_CYPHER_DISPLAY
+      });
+    },
+    launchNotification(message) {
+      dispatch(launchNotification(message));
+    },
+  }
+});
+
+const CypherQueryState = () => ({});
+
+export default withStyles(styles)(
+  connect(
+    CypherQueryState,
+    CypherQueryDispatch
+  )(CypherQuery)
+);
