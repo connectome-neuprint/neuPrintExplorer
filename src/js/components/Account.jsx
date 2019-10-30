b9c72d5b0503cd1f005634a4f01b59e4c2f325a0
Jody Clements <jody@neomorphic.co.uk>
2019-10-30 10:15:07 -0400

    Updates Account page to show admin level & avatar.

--- src/js/components/Account.jsx
+++ src/js/components/Account.jsx
@@ -4,22 +4,36 @@ import { connect } from 'react-redux';
 
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
 import { withStyles } from '@material-ui/core/styles';
 
 function Account(props) {
   const { user, classes } = props;
   return (
     <div className={classes.root}>
-      <Typography>User account for {user.get('userInfo').Email}</Typography>
-      <Typography>Auth Token:</Typography>
-      <Paper className={classes.token}>{user.get('token')}</Paper>
+      <Typography variant="h3">Account</Typography>
+      <Paper className={classes.account}>
+        <Typography>You are logged in as:</Typography>
+        <Typography>{user.get('userInfo').Email}</Typography>
+        <Avatar
+          alt={user.get('userInfo').Email}
+          src={user.get('userInfo').ImageURL}
+          className={classes.avatar}
+        />
+        <Typography>Authorization level:</Typography>
+        <Typography>{user.get('userInfo').AuthLevel}</Typography>
+      </Paper>
+      <Paper className={classes.token}>
+        <Typography>Auth Token:</Typography>
+        {user.get('token')}
+      </Paper>
     </div>
   );
 }
 
 Account.propTypes = {
   user: PropTypes.object.isRequired,
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -30,8 +44,15 @@ const styles = theme => ({
   root: {
     padding: `${theme.spacing.unit}px`
   },
+  avatar: {
+    margin: 10
+  },
+  account: {
+    padding: `${theme.spacing.unit * 2}px`,
+    marginBottom: `${theme.spacing.unit}px`
+  },
   token: {
-    padding:  `${theme.spacing.unit * 2}px`,
+    padding: `${theme.spacing.unit * 2}px`,
     wordBreak: 'break-all'
   }
 });
