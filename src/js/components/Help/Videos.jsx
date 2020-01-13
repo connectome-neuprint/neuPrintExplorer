290f9c07783195303651a7334f7b96d9b78636a0
Stephen Plaza <plazas@janelia.hhmi.org>
2020-01-12 23:46:49 -0500

    adds two tutorial videos to the help documentation

--- src/js/components/Help/Videos.jsx
+++ src/js/components/Help/Videos.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 const styles = () => ({
+  root: {
+    flexGrow: 1
+  },
   roottext: {
     flex: 1
   }
@@ -12,10 +16,34 @@ const styles = () => ({
 function HelpVideos(props) {
   const { classes } = props;
   return (
-    <div style={{ padding: 8 * 3, width: '100%' }}>
+    <div className={classes.root} style={{ padding: 8 * 3, width: '100%' }}>
       <div className={classes.roottext}>
         <Typography>Tutorial videos highlighting different aspects of neuPrintExplorer</Typography>
       </div>
+      <Grid container spacing={24}>
+        <Grid item xs>
+          <iframe
+            title="Getting started"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/-BHftRHDQtc"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </Grid>
+        <Grid item xs>
+          <iframe
+            title="Common connectivity query"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/VAtI6V86qlY"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 }
