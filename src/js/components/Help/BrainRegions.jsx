f1a59fcf1de5c72ce9ed1f745c8f39233e4dbab8
Jody Clements <jody@neomorphic.co.uk>
2020-02-18 15:29:36 -0500

    Removes annotation layer from brain region pdf.
    This layer was not being used and caused a large blank area to be
    rendered under the pdf.

--- src/js/components/Help/BrainRegions.jsx
+++ src/js/components/Help/BrainRegions.jsx
@@ -6,7 +6,7 @@ export default function HelpBrainRegion() {
   return (
     <div style={{margin: '1em'}} >
       <Document file="/public/brainregions.pdf">
-        <Page pageNumber={1} scale={1.5}/>
+        <Page pageNumber={1} scale={1.5} renderAnnotationLayer={false}/>
       </Document>
     </div>
   );
