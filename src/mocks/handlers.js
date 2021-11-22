498d054177ebb9466d881d41dc4bb1aad1a39268
Jody Clements <jody@neomorphic.co.uk>
2021-11-22 14:53:44 -0500

    Adds mock handler for vnc neuroglancer layers

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,17 +1,20 @@
 import { rest } from 'msw';
+import ngLayers from './ng-layers.json';
 
 /* eslint-disable-next-line import/prefer-default-export */
 export const handlers = [
   // Handles a POST /login request
   rest.post('/login', (req, res, ctx) => {
     // Persist user's authentication in the session
-    sessionStorage.setItem('is-authenticated', 'true')
+    sessionStorage.setItem('is-authenticated', 'true');
     return res(
       // Respond with a 200 status code
-      ctx.status(200),
-    )
+      ctx.status(200)
+    );
   }),
-
+  rest.get('/api/npexplorer/nglayers/vnc.json', (req, res, ctx) =>
+    res(ctx.status(200), ctx.json(ngLayers))
+  ),
   // Handles a GET /user request
   rest.get('/user', null)
 ];
